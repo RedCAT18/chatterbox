aac45104bf662e1d3ae1109f36e64b84601df224
RedCAT <akaineko18@gmail.com>
2019-07-02 15:58:47 +1200

    update websocket

--- websocket-server.js
+++ websocket-server.js
@@ -6,14 +6,27 @@ var ws = new WebSocketServer({
   port: port
 });
 
+//array to store messages
+var messages = [];
+
 console.log('Websockets server started.');
 
 //connection event handler
 ws.on('connection', function(socket) {
   console.log('Client connection established.');
 
+  messages.forEach(function(msg) {
+    socket.send(msg);
+  });
+
   socket.on('message', function(data) {
     console.log('Message received: ' + data);
-    socket.send(data);
+    messages.push(data);
+
+    //send message to all users
+    ws.clients.forEach(function(clientSocket) {
+      clientSocket.send(data);
+    });
+    // socket.send(data);
   });
 });
